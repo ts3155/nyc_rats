01b47ee96fe173ab550c01f9eb66d03d516edae1
ts3155 <62108760+ts3155@users.noreply.github.com>
2020-03-26 12:16:35 -0400

    Create map.js

---
+++ map.js
@@ -0,0 +1,38 @@
+'use strict'
+
+console.log('Loaded map.js')
+
+mapboxgl.accessToken = 'YOUR TOKEN HERE'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/ts3155/ck88x3nvc1kiq1ikltm993b2y',
+    center: [-73.96216,40.80779],
+    zoom: 16
+})
+
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+map.addControl(navigation, 'top-left')
+
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+map.addControl(scale, 'bottom-right')
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+map.addControl(geolocate, 'top-left')
+
+geolocate.on('geolocate', function(event) {
+
+})
